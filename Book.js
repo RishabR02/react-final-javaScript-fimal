6726c500c7af1571627321a645ac9ca4f8742582
RishabR02 <shettyrishab10@gmail.com>
2025-12-03 06:47:52 +0530

    Create Book.js

---
+++ Book.js
@@ -0,0 +1,19 @@
+
+// Student Profile Manager
+let student = {
+name: "Akhil"
+,
+age: 20,
+marks: [80, 85, 90],
+avg: function() {
+let t = 0;
+for (let m of this.marks) t += m;
+return t / this.marks.length;
+}
+};
+
+for (let x in student)
+console.log(x + ":"
+, student[x]);
+console.log("Avg:"
+, student.avg());
